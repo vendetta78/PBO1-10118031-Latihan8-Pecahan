a9a20ef838b77cc0f64c26af908a954337c3ac07
vendetta78 <55823091+vendetta78@users.noreply.github.com>
2019-09-26 12:01:45 +0700

    Add files via upload

---
+++ PBO1-10118031-Latihan8-Pecahan/src/pbo1/pkg10118031/latihan8/pecahan/PBO110118031Latihan8Pecahan.java
@@ -0,0 +1,24 @@
+package pbo1.pkg10118031.latihan8.pecahan;
+
+/**
+ *
+ * @author Vendetta
+ * NAMA              : Rizki Aria Putra
+ * KELAS             : PBO1
+ * NIM               : 10118031
+ * Deskripsi Program : Program ini berisi program untuk 
+ *                     varibel tipedata Float dan double
+ */
+
+public class PBO110118031Latihan8Pecahan {
+
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789f;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+
+}
